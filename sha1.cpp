8b3f1b927dd8b3c2605297d6d578c97e27e1ab68
micael <micael@hildenborg.com>
2009-04-22 19:10:13 +0000

    Added license comment in sources

--- sha1.cpp
+++ sha1.cpp
@@ -1,11 +1,34 @@
 /*
-Fast and memory optimized SHA1.
-Originally made by Micael Hildenborg 2009.
-modified by Gustav 2009
-modified by Micael Hildenborg 2009 from a lot of input from members of the gamedev.se forum.
- 
-Reference material for SHA1 (and more):
-http://en.wikipedia.org/wiki/SHA_hash_functions
+Copyright (c) 2009, Micael Hildenborg
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of Micael Hildenborg nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY Micael Hildenborg ''AS IS'' AND ANY
+EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL Micael Hildenborg BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+/*
+Contributors:
+Gustav
+Several members in the gamedev.se forum.
 */
  
 #include "sha1.h"
